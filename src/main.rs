01b761468510ba4eefd2fed091f542716abd0b97
sunjesse <jessera@outlook.com>
2025-02-04 23:33:53 -0500

    add demo gif

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() -> std::io::Result<()> {
     let mut trie: Trie = Trie::new(); 
     reader::read_from_file("./src/data/words.txt", &mut trie);
 
-    println!("LEN IS {:?}", trie.len());
+    println!("Vocab length is: {:}.", trie.len());
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     
@@ -32,24 +32,30 @@ fn main() -> std::io::Result<()> {
                         } else {
                             input.push(c);
                         }
-                        println!("\r{} {}", input_words.join(" "), input);
-                        stdout.flush()?;
                     }
                     KeyCode::Backspace => {
                         if !input.is_empty() {
                             input.pop();
                             stdout.flush()?;
-                        } else if !input_words.is_empty() {
-                            input = input_words.pop().expect("did not pop");
+                        } else if let Some(w) = input_words.pop() {
+                            input = w;
                         }
-                        println!("\r{} {}", input_words.join(" "), input);
                     }
                     KeyCode::Esc => break,
                     _ => {}
                 }
+                // print suggestions
                 if let Some(words) = trie.get_top_k(input.clone(), 10) {
                     println!("\r{:?}", words);
                 }
+
+                // print buffered words
+                if input_words.is_empty() {
+                    println!("\r{}", input);
+                } else {
+                    println!("\r{} {}", input_words.join(" "), input);
+                }
+                stdout.flush()?;
             }
         }
     }
